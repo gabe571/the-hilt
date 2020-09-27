908c8510d4d80605877db796a3f5f0236f04f349
Gabe571 <gabriel.castro1028@gmail.com>
2020-09-27 13:34:31 -0700

    fetch working

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationsController < ApplicationController
-end
+    def index
+        applications = Application.all 
+        render json: applications
+    end
+        
+    def show
+        application = Application.find(params[:id])
+        render json: application
+    end
+
+    def create
+        @application = Application.new(application_params)
+        @application.save
+        render json: application
+     end
+
+     def application_params
+        params.require(:application).permit(:content)
+     end
+    end
--- app/controllers/guilds_controller.rb
+++ app/controllers/guilds_controller.rb
@@ -1,2 +1,35 @@
 class GuildsController < ApplicationController
+   
+    def index
+        guilds = Guild.all 
+        render json: guilds
+    end
+
+    def show
+        guild = Guild.find_by(params[:id])
+        render json: guild
+    end
+
+    def create
+        guild = Guild.create(guild_params)
+        render json: guild
+    end
+
+    def update
+        guild = Guild.find(params[:id])
+        guild.save
+        render json: guild
+    end
+
+    def destroy
+        guild = Guild.find(params[:id])
+        guild.destroy
+        render json: {message: "Successfully removed guild"}
+    end
+
+    private
+
+    def guild_params
+        params.require(:guild).permit(:name, :realm, :faction, :img_url, :description)
+    end
 end
