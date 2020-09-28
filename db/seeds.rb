0c328b68626ca1a7d00112beb4df27b5da4ef792
Gabe571 <gabriel.castro1028@gmail.com>
2020-09-27 17:35:15 -0700

    working

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,10 +11,10 @@ Application.destroy_all
 #   Character.create(name: 'Luke', movie: movies.first)
 
     guilds = [
-    {name: "Jakes MDI Team", realm: "Illidan", faction: "Horde", img_url: "https://akns-images.eonline.com/eol_images/Entire_Site/2019027/rs_600x600-190127175953-600.2019-sag-awards-red-carpet-fashions-john-krasinsk.jpg?fit=around|1080:1080&output-quality=90&crop=1080:1080;center,top", description: "keys keys keys"},
-    {name: "Team Reckless", realm: "Sargaras", faction: "Horde", img_url: "https://akns-images.eonline.com/eol_images/Entire_Site/2019027/rs_600x600-190127175953-600.2019-sag-awards-red-carpet-fashions-john-krasinsk.jpg?fit=around|1080:1080&output-quality=90&crop=1080:1080;center,top", description: "Attempting to attempt"},
-    {name: "The Fighting 9th!", realm: "Rachet", faction: "Horde", img_url: "https://akns-images.eonline.com/eol_images/Entire_Site/2019027/rs_600x600-190127175953-600.2019-sag-awards-red-carpet-fashions-john-krasinsk.jpg?fit=around|1080:1080&output-quality=90&crop=1080:1080;center,top", description: "MASS!"},
-    {name: "Ohms", realm: "Black Dragon Flight", faction: "Horde", img_url: "https://akns-images.eonline.com/eol_images/Entire_Site/2019027/rs_600x600-190127175953-600.2019-sag-awards-red-carpet-fashions-john-krasinsk.jpg?fit=around|1080:1080&output-quality=90&crop=1080:1080;center,top", description: "Music!"}
+    {name: "Jakes MDI Team", realm: "Illidan", faction: "Horde", img_url: "https://i.pinimg.com/originals/43/d2/60/43d26064313e583249b17554524b808b.jpg", description: "keys keys keys"},
+    {name: "Team Reckless", realm: "Sargaras", faction: "Horde", img_url: "https://i.pinimg.com/originals/43/d2/60/43d26064313e583249b17554524b808b.jpg"},
+    {name: "The Fighting 9th!", realm: "Rachet", faction: "Horde", img_url: "https://i.pinimg.com/originals/43/d2/60/43d26064313e583249b17554524b808b.jpg", description: "MASS!"},
+    {name: "Ohms", realm: "Black Dragon Flight", faction: "Horde", img_url: "https://i.pinimg.com/originals/43/d2/60/43d26064313e583249b17554524b808b.jpg", description: "Music!"}
     ]
 
     guilds.each {|guild| Guild.create(guild)}
