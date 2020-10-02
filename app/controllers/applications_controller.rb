32e04dd5043f216472eac03177173e60b41cbce3
Gabe571 <gabriel.castro1028@gmail.com>
2020-10-01 21:03:31 -0700

    working backend for application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -12,10 +12,17 @@ class ApplicationsController < ApplicationController
     def create
         @application = Application.new(application_params)
         @application.save
-        render json: application
+        render json: @application
      end
 
+     def update
+        application = Application.find(params[:id])
+        application.update(application_params)
+        application.save
+        render json: application
+    end
+
      def application_params
-        params.require(:application).permit(:content)
+        params.require(:application).permit(:content, :guild_id, :user_id)
      end
     end
